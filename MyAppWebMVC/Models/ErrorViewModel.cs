74658c86ba4d194a1c4b03258bdfe2ccc98dfc58
Alfred301174 <137465741+Alfred301174@users.noreply.github.com>
2025-03-03 11:33:22 -0600

    Add project files.

---
+++ MyAppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
